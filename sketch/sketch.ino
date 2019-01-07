ad91eb31098c2840e7be5e1393c1b56c8fbfc252
Michael Essmyer <mde9rb@mail.umkc.edu>
2019-01-06 22:44:06 -0600

    Update sketch.ino
    Testing MotorDriver class

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,5 @@
 // LIBRARIES
-
+#include "MotorDriver.h"
 // ENCODER PINS
 
 // MOTOR PINS
@@ -17,12 +17,26 @@
 // MOTORS
 
 // MOTOR CONTROL
-
+MotorDriver MD;
 
 void setup() {
 	
 }
 
 void loop() {
-	
+	runMotors(1, 255, 20, 1000);
+  stopMotors(1000);
 }
+
+void runMotors(int Dir, int R_Pwm, int L_Pwm, int time){
+    MD.SetR_MPower(R_Pwm);
+    MD.SetR_Direction(Dir);
+    MD.SetL_MPower(L_Pwm);
+    MD.SetL_Direction(-Dir);
+    delay(time);
+  }
+void stopMotors(int time){
+  MD.SetR_MPower(0);
+  MD.SetL_MPower(0);
+  delay(time);
+  }
